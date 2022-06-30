797b27b900ca5978482b59411de89e40a2ada0fa
Zeel Patel <zeel.c.patel@gmail.com>
2022-06-30 11:36:27 -0400

    Sanity Setup

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+	return (
+		<header>
+			<div>
+                <link href="/">
+                    <img src=""
+                </link>
+			</div>
+			<div></div>
+		</header>
+	)
+}
+
+export default Header
